d75fceb9ef5ec5be7a59dd28498d53f8f4cb0cac
yashpaneliya <yashsp1158@gmail.com>
2023-01-17 17:55:38 +0530

    Match data formatted

--- A2/task/round16.py
+++ A2/task/round16.py
@@ -1,6 +1,11 @@
 import ply.yacc as yacc
 import ply.lex as lex
 
+matchlist = []
+scorerlist = []
+matchdetails = []
+rightpenscorerslist = []
+
 tokens=[
     'OPENROUND', 'ENDROUND', 'OPENMATCHDIV',
     'OPENPOINTTABLE', 'CLOSEPOINTTABLE', 'OPENTABLE', 'OPENLIST', 'CLOSELIST', 'OPENROW', 'CLOSEROW', 'OPENDATA', 'CLOSEDATA', 'OPENHEADER', 'CLOSEHEADER', 'OPENHREF', 'CLOSEHREF', 'SEPARATOR',
@@ -14,10 +19,6 @@ def t_OPENROUND(t):
 def t_ENDROUND(t):
     '''<span\sclass="mw-headline"\sid="Quarter-finals">Quarter\-finals</span>'''
     return t
-
-# def t_OPENPOINTTABLE(t):
-#     '''<table\sclass="wikitable"\sstyle="text-align:center;">'''
-#     return t
 
 def t_OPENTABLE(t):
     '''<table\sclass="fevent">'''
@@ -110,25 +111,26 @@ def p_handlematches(p):
                      | OPENMATCHDIV skip OPENTABLE OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CONTENT CLOSEHEADER OPENHEADER OPENHREF CONTENT CLOSEHREF skip CLOSEHEADER OPENHEADER CONTENT OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENDATA skip CLOSEDATA OPENDATA handlescorer CLOSEDATA CLOSEROW OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENHEADER CONTENT CLOSEHEADER OPENDATA handlepenscorer CLOSEDATA CLOSEROW CLOSEPOINTTABLE FRIGHTDIV handledetails
                      | '''
     if len(p) > 1 and len(p) < 45:
-        print(p[7],p[13],p[19])
+        matchlist.append(p[7]+" "+p[13]+" "+p[19])
     elif len(p) > 45:
-        print(p[7],p[13],p[20])
+        matchlist.append(p[7]+" "+p[13]+" "+p[20])
 
 def p_handlescorer(p):
     '''handlescorer : skip OPENLIST OPENHREF CONTENT CLOSEHREF skip CLOSELIST handlescorer
                     | '''
     if len(p)>1:
-        print(p[4])
+        scorerlist.append(p[4])
 
 def p_handlepenscorer(p):
     '''handlepenscorer : skip OPENLIST CONTENT OPENHREF CONTENT CLOSEHREF skip CLOSELIST handlepenscorer
                     | '''
     if len(p)>1:
-        print(p[5])
+        rightpenscorerslist.append(p[5])
 
 def p_handledetails(p):
     '''handledetails : OPENHREF CONTENT CLOSEHREF CONTENT OPENHREF CONTENT CLOSEHREF CONTENT CONTENT OPENHREF CONTENT CLOSEHREF skip'''
-    print(p[2],p[4],p[6],p[8],p[9],p[11])
+    # print(p[2],p[4],p[6],p[8],p[9],p[11])
+    matchdetails.append(p[2]+" "+p[4]+" "+p[6]+" "+p[8]+" "+p[9]+" "+p[11])
 
 def p_before(p):
     '''before : CONTENT before
@@ -157,22 +159,77 @@ def p_skip(p):
             | '''
 
 def p_error(p):
-    print("Error ",p)
     pass
 
-if __name__ == "__main__":
+def round16():
     lexer = lex.lex()
     parser = yacc.yacc()
     f=open('fifa.html','r',encoding='utf-8')
     data = f.read()
     lexer.input(data)
-    log = open('log.txt','w', encoding='utf8')
-    while True:
-        tok = lexer.token()
-        if not tok:
-            break
-        log.write(str(tok)+'\n')
-    log.close()
     res = parser.parse(data)
-    # print(res)
     f.close()
+    matchlist.reverse()
+    round16Data = {
+        'matchlist' : matchlist,
+        '1':{
+            'score' : matchlist[0],
+            'details' : matchdetails[0],
+            'scorerlist' : [scorerlist[0],scorerlist[1],scorerlist[2],scorerlist[3]],
+            'isPen' : False,
+            'penscorers' : [],
+        },
+        '2':{
+            'score' : matchlist[1],
+            'details' : matchdetails[1],
+            'scorerlist' : [scorerlist[4],scorerlist[5],scorerlist[6]],
+            'isPen' : False,
+            'penscorers' : [],
+        },
+        '3':{
+            'score' : matchlist[2],
+            'details' : matchdetails[2],
+            'scorerlist' : [scorerlist[7],scorerlist[8],scorerlist[9]],
+            'isPen' : False,
+            'penscorers' : [],
+        },
+        '4':{
+            'score' : matchlist[3],
+            'details' : matchdetails[3],
+            'scorerlist' : [scorerlist[10],scorerlist[11],scorerlist[12]],
+            'isPen' : False,
+            'penscorers' : [],
+        },
+        '5':{
+            'score' : matchlist[4],
+            'details' : matchdetails[4],
+            'scorerlist' : [scorerlist[13],scorerlist[14]],
+            'isPen' : True,
+            'penscorers' : [rightpenscorerslist[0],rightpenscorerslist[1],rightpenscorerslist[2],rightpenscorerslist[3],scorerlist[15],scorerlist[16],scorerlist[17],scorerlist[18]],
+        },
+        '6':{
+            'score' : matchlist[5],
+            'details' : matchdetails[5],
+            'scorerlist' : [scorerlist[19],scorerlist[20],scorerlist[21],scorerlist[22],scorerlist[23]],
+            'isPen' : False,
+            'penscorers' : [],
+        },
+        '7':{
+            'score' : matchlist[6],
+            'details' : matchdetails[6],
+            'scorerlist' : [],
+            'isPen' : True,
+            'penscorers' : [rightpenscorerslist[4],rightpenscorerslist[5],rightpenscorerslist[6],scorerlist[24],scorerlist[25],scorerlist[26],scorerlist[27]],
+        },
+        '8':{
+            'score' : matchlist[7],
+            'details' : matchdetails[7],
+            'scorerlist' : [scorerlist[28],scorerlist[29],scorerlist[30],scorerlist[31],scorerlist[32]],
+            'isPen' : False,
+            'penscorers' : [],
+        },
+    }
+    print(round16Data)
+    return round16Data
+
+round16()
--- A2/task/thirdplace.py
+++ A2/task/thirdplace.py
@@ -1,178 +1,240 @@
 import ply.yacc as yacc
 import ply.lex as lex
 
-tokens=[
-    'OPENTHIRD', 'CLOSETHIRD', 'OPENMATCHDIV',
-    'OPENPOINTTABLE', 'CLOSEPOINTTABLE', 'OPENTABLE', 'OPENLIST', 'CLOSELIST', 'OPENROW', 'CLOSEROW', 'OPENDATA', 'CLOSEDATA', 'OPENHEADER', 'CLOSEHEADER', 'OPENHREF', 'CLOSEHREF', 'SEPARATOR',
-    'CONTENT', 'GARBAGE', 'OPENLINK', 'OPENABBR', 'CLOSEABBR', 'FRIGHTDIV'
+matchlist = []
+scorerlist = []
+matchdetails = []
+rightpenscorerslist = []
+
+tokens = [
+    "OPENTHIRD",
+    "CLOSETHIRD",
+    "OPENMATCHDIV",
+    "OPENPOINTTABLE",
+    "CLOSEPOINTTABLE",
+    "OPENTABLE",
+    "OPENLIST",
+    "CLOSELIST",
+    "OPENROW",
+    "CLOSEROW",
+    "OPENDATA",
+    "CLOSEDATA",
+    "OPENHEADER",
+    "CLOSEHEADER",
+    "OPENHREF",
+    "CLOSEHREF",
+    "SEPARATOR",
+    "CONTENT",
+    "GARBAGE",
+    "OPENLINK",
+    "OPENABBR",
+    "CLOSEABBR",
+    "FRIGHTDIV",
 ]
 
+
 def t_OPENTHIRD(t):
-    '''<span\sclass="mw-headline"\sid="Third_place_play-off">Third\splace\splay\-off</span>'''
+    """<span\sclass="mw-headline"\sid="Third_place_play-off">Third\splace\splay\-off</span>"""
     return t
 
+
 def t_CLOSETHIRD(t):
-    '''<span\sclass="mw-headline"\sid="Final">Final</span>'''
+    """<span\sclass="mw-headline"\sid="Final">Final</span>"""
     return t
+
 
 # def t_OPENPOINTTABLE(t):
 #     '''<table\sclass="wikitable"\sstyle="text-align:center;">'''
 #     return t
 
+
 def t_OPENTABLE(t):
-    '''<table\sclass="fevent">'''
+    """<table\sclass="fevent">"""
     return t
+
 
 def t_CLOSEPOINTTABLE(t):
-    '''</table>'''
+    """</table>"""
     return t
+
 
 def t_OPENMATCHDIV(t):
-    '''<div\sitemscope=""\sitemtype="http&\#58;//schema.org/SportsEvent"\sclass="footballbox">'''
+    """<div\sitemscope=""\sitemtype="http&\#58;//schema.org/SportsEvent"\sclass="footballbox">"""
     return t
+
 
 def t_FRIGHTDIV(t):
-    '''<div\sclass="fright">'''
+    """<div\sclass="fright">"""
     return t
+
 
 def t_SEPARATOR(t):
-    '''<link\srel="mw-deduplicated-inline-style"\shref="mw-data:TemplateStyles:r997937747"/>'''
+    """<link\srel="mw-deduplicated-inline-style"\shref="mw-data:TemplateStyles:r997937747"/>"""
     return t
+
 
 def t_OPENROW(t):
-    r'<tr.*?>'
+    r"<tr.*?>"
     return t
+
 
 def t_CLOSEROW(t):
-    r'</tr>'
+    r"</tr>"
     return t
+
 
 def t_OPENDATA(t):
-    r'<td.*?>'
+    r"<td.*?>"
     return t
+
 
 def t_CLOSEDATA(t):
-    r'</td>'
+    r"</td>"
     return t
+
 
 def t_OPENLINK(t):
-    r'<link.*?>'
+    r"<link.*?>"
+
 
 def t_OPENABBR(t):
-    r'<abbr.*?>'
+    r"<abbr.*?>"
+
 
 def t_CLOSEABBR(t):
-    r'</abbr>'   
+    r"</abbr>"
+
 
 def t_OPENLIST(t):
-    r'<li .*?>'
+    r"<li .*?>"
     return t
+
 
 def t_CLOSELIST(t):
-    r'</li>'
+    r"</li>"
     return t
+
 
 def t_OPENHREF(t):
-    r'<a .*?>'
+    r"<a .*?>"
     return t
+
 
 def t_CLOSEHREF(t):
-    r'</a>'
+    r"</a>"
     return t
+
 
 def t_OPENHEADER(t):
-    r'<th.*?>'
+    r"<th.*?>"
     return t
 
+
 def t_CLOSEHEADER(t):
-    r'</th>'
+    r"</th>"
     return t
+
 
 def t_CONTENT(t):
     r'[A-Za-z0-9ñáãćéíøäæóōúńÁÉÜÚÑğÓÍïüšëčýÿû,$#§&.+\-:;@ \'")\(–]+'
     return t
-	
+
+
 def t_GARBAGE(t):
-    r'<.*?>'
+    r"<.*?>"
+
 
 def t_error(t):
     t.lexer.skip(1)
 
+
 def p_start(p):
-    '''start : before OPENTHIRD skip SEPARATOR handlematches CLOSETHIRD'''
+    """start : before OPENTHIRD skip SEPARATOR handlematches CLOSETHIRD"""
+
 
 def p_handlematches(p):
-    '''handlematches : skip OPENTABLE OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CONTENT CLOSEHEADER OPENHEADER OPENHREF CONTENT CLOSEHREF CLOSEHEADER OPENHEADER CONTENT OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENDATA skip CLOSEDATA OPENDATA handlescorer CLOSEDATA CLOSEROW CLOSEPOINTTABLE FRIGHTDIV handledetails SEPARATOR handlematches
-                     | OPENMATCHDIV skip OPENTABLE OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CONTENT CLOSEHEADER OPENHEADER OPENHREF CONTENT CLOSEHREF CLOSEHEADER OPENHEADER CONTENT OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENDATA skip CLOSEDATA OPENDATA handlescorer CLOSEDATA CLOSEROW CLOSEPOINTTABLE FRIGHTDIV handledetails SEPARATOR handlematches
-                     | OPENMATCHDIV skip OPENTABLE OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CONTENT CLOSEHEADER OPENHEADER OPENHREF CONTENT CLOSEHREF CLOSEHEADER OPENHEADER CONTENT OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENDATA skip CLOSEDATA OPENDATA handlescorer CLOSEDATA CLOSEROW CLOSEPOINTTABLE FRIGHTDIV handledetails
-                     | skip OPENTABLE OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CONTENT CLOSEHEADER OPENHEADER OPENHREF CONTENT CLOSEHREF skip CLOSEHEADER OPENHEADER CONTENT OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENDATA skip CLOSEDATA OPENDATA handlescorer CLOSEDATA CLOSEROW OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENHEADER CONTENT CLOSEHEADER OPENDATA handlepenscorer CLOSEDATA CLOSEROW CLOSEPOINTTABLE FRIGHTDIV handledetails SEPARATOR handlematches
-                     | OPENMATCHDIV skip OPENTABLE OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CONTENT CLOSEHEADER OPENHEADER OPENHREF CONTENT CLOSEHREF skip CLOSEHEADER OPENHEADER CONTENT OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENDATA skip CLOSEDATA OPENDATA handlescorer CLOSEDATA CLOSEROW OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENHEADER CONTENT CLOSEHEADER OPENDATA handlepenscorer CLOSEDATA CLOSEROW CLOSEPOINTTABLE FRIGHTDIV handledetails SEPARATOR handlematches
-                     | OPENMATCHDIV skip OPENTABLE OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CONTENT CLOSEHEADER OPENHEADER OPENHREF CONTENT CLOSEHREF skip CLOSEHEADER OPENHEADER CONTENT OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENDATA skip CLOSEDATA OPENDATA handlescorer CLOSEDATA CLOSEROW OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENHEADER CONTENT CLOSEHEADER OPENDATA handlepenscorer CLOSEDATA CLOSEROW CLOSEPOINTTABLE FRIGHTDIV handledetails
-                     | '''
+    """handlematches : skip OPENTABLE OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CONTENT CLOSEHEADER OPENHEADER OPENHREF CONTENT CLOSEHREF CLOSEHEADER OPENHEADER CONTENT OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENDATA skip CLOSEDATA OPENDATA handlescorer CLOSEDATA CLOSEROW CLOSEPOINTTABLE FRIGHTDIV handledetails SEPARATOR handlematches
+    | OPENMATCHDIV skip OPENTABLE OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CONTENT CLOSEHEADER OPENHEADER OPENHREF CONTENT CLOSEHREF CLOSEHEADER OPENHEADER CONTENT OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENDATA skip CLOSEDATA OPENDATA handlescorer CLOSEDATA CLOSEROW CLOSEPOINTTABLE FRIGHTDIV handledetails SEPARATOR handlematches
+    | OPENMATCHDIV skip OPENTABLE OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CONTENT CLOSEHEADER OPENHEADER OPENHREF CONTENT CLOSEHREF CLOSEHEADER OPENHEADER CONTENT OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENDATA skip CLOSEDATA OPENDATA handlescorer CLOSEDATA CLOSEROW CLOSEPOINTTABLE FRIGHTDIV handledetails
+    | skip OPENTABLE OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CONTENT CLOSEHEADER OPENHEADER OPENHREF CONTENT CLOSEHREF skip CLOSEHEADER OPENHEADER CONTENT OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENDATA skip CLOSEDATA OPENDATA handlescorer CLOSEDATA CLOSEROW OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENHEADER CONTENT CLOSEHEADER OPENDATA handlepenscorer CLOSEDATA CLOSEROW CLOSEPOINTTABLE FRIGHTDIV handledetails SEPARATOR handlematches
+    | OPENMATCHDIV skip OPENTABLE OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CONTENT CLOSEHEADER OPENHEADER OPENHREF CONTENT CLOSEHREF skip CLOSEHEADER OPENHEADER CONTENT OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENDATA skip CLOSEDATA OPENDATA handlescorer CLOSEDATA CLOSEROW OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENHEADER CONTENT CLOSEHEADER OPENDATA handlepenscorer CLOSEDATA CLOSEROW CLOSEPOINTTABLE FRIGHTDIV handledetails SEPARATOR handlematches
+    | OPENMATCHDIV skip OPENTABLE OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CONTENT CLOSEHEADER OPENHEADER OPENHREF CONTENT CLOSEHREF skip CLOSEHEADER OPENHEADER CONTENT OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENDATA skip CLOSEDATA OPENDATA handlescorer CLOSEDATA CLOSEROW OPENROW OPENHEADER OPENHREF CONTENT CLOSEHREF CLOSEHEADER CLOSEROW OPENROW OPENDATA handlescorer CLOSEDATA OPENHEADER CONTENT CLOSEHEADER OPENDATA handlepenscorer CLOSEDATA CLOSEROW CLOSEPOINTTABLE FRIGHTDIV handledetails
+    |"""
     if len(p) > 1 and len(p) < 45:
-        print(p[7],p[13],p[19])
+        matchlist.append(p[7] + " " + p[13] + " " + p[19])
     elif len(p) > 45:
-        print(p[7],p[13],p[20])
+        matchlist.append(p[7] + " " + p[13] + " " + p[20])
+
 
 def p_handlescorer(p):
-    '''handlescorer : skip OPENLIST OPENHREF CONTENT CLOSEHREF skip CLOSELIST handlescorer
-                    | '''
-    if len(p)>1:
-        print(p[4])
+    """handlescorer : skip OPENLIST OPENHREF CONTENT CLOSEHREF skip CLOSELIST handlescorer
+    |"""
+    if len(p) > 1:
+        scorerlist.append(p[4])
+
 
 def p_handlepenscorer(p):
-    '''handlepenscorer : skip OPENLIST CONTENT OPENHREF CONTENT CLOSEHREF skip CLOSELIST handlepenscorer
-                    | '''
-    if len(p)>1:
-        print(p[5])
+    """handlepenscorer : skip OPENLIST CONTENT OPENHREF CONTENT CLOSEHREF skip CLOSELIST handlepenscorer
+    |"""
+    if len(p) > 1:
+        rightpenscorerslist.append(p[5])
+
 
 def p_handledetails(p):
-    '''handledetails : OPENHREF CONTENT CLOSEHREF CONTENT OPENHREF CONTENT CLOSEHREF CONTENT CONTENT OPENHREF CONTENT CLOSEHREF skip'''
-    print(p[2],p[4],p[6],p[8],p[9],p[11])
+    """handledetails : OPENHREF CONTENT CLOSEHREF CONTENT OPENHREF CONTENT CLOSEHREF CONTENT CONTENT OPENHREF CONTENT CLOSEHREF skip"""
+    matchdetails.append(
+        p[2] + " " + p[4] + " " + p[6] + " " + p[8] + " " + p[9] + " " + p[11]
+    )
+
 
 def p_before(p):
-    '''before : CONTENT before
-              | OPENHREF before 
-              | CLOSEHREF before
-              | OPENHEADER before
-              | CLOSEHEADER before
-              | OPENDATA before
-              | CLOSEDATA before
-              | OPENROW before
-              | CLOSEROW before
-              | OPENLIST before
-              | CLOSELIST before
-              | OPENTABLE before
-              | OPENLINK before
-              | CLOSEPOINTTABLE before
-              | OPENMATCHDIV before
-              | FRIGHTDIV before
-              | SEPARATOR before
-              | ''' 
+    """before : CONTENT before
+    | OPENHREF before
+    | CLOSEHREF before
+    | OPENHEADER before
+    | CLOSEHEADER before
+    | OPENDATA before
+    | CLOSEDATA before
+    | OPENROW before
+    | CLOSEROW before
+    | OPENLIST before
+    | CLOSELIST before
+    | OPENTABLE before
+    | OPENLINK before
+    | CLOSEPOINTTABLE before
+    | OPENMATCHDIV before
+    | FRIGHTDIV before
+    | SEPARATOR before
+    |"""
+
 
 def p_skip(p):
-    '''skip : CONTENT skip
-            | OPENHREF skip
-            | CLOSEHREF skip
-            | '''
+    """skip : CONTENT skip
+    | OPENHREF skip
+    | CLOSEHREF skip
+    |"""
+
 
 def p_error(p):
-    print("Error ",p)
     pass
 
-if __name__ == "__main__":
+
+def thirdPlace():
     lexer = lex.lex()
     parser = yacc.yacc()
-    f=open('fifa.html','r',encoding='utf-8')
+    f = open("fifa.html", "r", encoding="utf-8")
     data = f.read()
     lexer.input(data)
-    log = open('log.txt','w', encoding='utf8')
-    while True:
-        tok = lexer.token()
-        if not tok:
-            break
-        log.write(str(tok)+'\n')
-    log.close()
     res = parser.parse(data)
-    # print(res)
+    thirdPlaceData = {
+        "matchlist": matchlist,
+        "1": {
+            "score": matchlist[0],
+            "details": matchdetails[0],
+            "scorerlist": [scorerlist[0], scorerlist[1],scorerlist[2]],
+            "isPen": False,
+            "penscorers": [],
+        },
+    }
     f.close()
+    return thirdPlaceData
